6dc47429794606cbdc5512b14f0f56f7af0e4540
maldron <bagjinhyeong640@gmail.com>
2023-09-21 14:59:03 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	 let firstName = 'park';
+	 let lastName = 'jinhyeong';
+	 let beltColor = 'red';
+
+	 const handleClick = ()   => {
+		beltColor = 'orange';
+	 };
+
+	 const handleInput = (e) => {
+		beltColor = e.target.value
+	 };
+</script>
+
+<main>
+	<p>{firstName} {lastName} - {beltColor} belt</p>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
+	<input type="text" bind:value={beltColor}>
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
